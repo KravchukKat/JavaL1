273e0c41fc867fc2414757ddabe8c1d8ca017bea
KravchukKat <ka.tarina@mail.ru>
2025-03-27 21:48:55 +0300

    done!!

--- src/test/java/Java1Tests.java
+++ src/test/java/Java1Tests.java
@@ -3,10 +3,10 @@ import java.nio.DoubleBuffer;
 public class Java1Tests {
     public static void main(String[] args) {
 
-        int x = 10;
-        int y = 20;
-        int k = 10;
-        double zDouble = 0.5;
+        int a = 10;
+        int b = 20;
+        int c = 10;
+        double d = 0.5;
 
         float maxValueFloat = Float.MAX_VALUE;
         float minValueFloat = Float.MIN_VALUE;
@@ -21,19 +21,19 @@ public class Java1Tests {
         int OverValueInteger = Integer.MAX_VALUE + 1;
 
         //Применить несколько арифметических операций ( + , -, * , /) над двумя примитивами типа int
-        System.out.println("Результат сложения: " + (x + y));
-        System.out.println("Результат вычитания: " + (x - y));
-        System.out.println("Результат умножения: " + (x * y));
-        System.out.println("Результат деления: " + (y / x));
+        System.out.println("Результат сложения: " + (a + b));
+        System.out.println("Результат вычитания: " + (a - b));
+        System.out.println("Результат умножения: " + (a * b));
+        System.out.println("Результат деления: " + (a / b));
         //Применить несколько арифметических операций над int и double в одном выражении
-        System.out.println("Результат сложения int + double: " + (zDouble + x));
-        System.out.println("Результат умножения int + double: " + (zDouble * x));
+        System.out.println("Результат сложения int + double: " + (d + a));
+        System.out.println("Результат умножения int + double: " + (d * a));
         //Применить несколько логических операций ( < , >, >=, <= )
-        System.out.println("Проверка boolean, что 0.5 меньше 10 " + (zDouble < x));
-        System.out.println("Проверка boolean, что 0.5 больше 10 " + (zDouble > x));
-        System.out.println("Проверка boolean, что 0.5 >= 10 " + (zDouble >= x));
-        System.out.println("Проверка boolean, что 0.5 <= 10 " + (zDouble <= x));
-        System.out.println("Проверка boolean, что 10 == 10 " + (k == x));
+        System.out.println("Проверка boolean, что 0.5 меньше 10 " + (d < a));
+        System.out.println("Проверка boolean, что 0.5 больше 10 " + (d > a));
+        System.out.println("Проверка boolean, что 0.5 >= 10 " + (d >= a));
+        System.out.println("Проверка boolean, что 0.5 <= 10 " + (d <= a));
+        System.out.println("Проверка boolean, что 10 == 10 " + (c == a));
         //Максимальный и минимальный диапазон
         System.out.println("Максимальное значение float: " + maxValueFloat);
         System.out.println("Минимальное значение float: " + minValueFloat);
@@ -42,9 +42,9 @@ public class Java1Tests {
         System.out.println("Максимальное значение int: " + maxValueInteger);
         System.out.println("Минимальное значение int: " + minValueInteger);
         //Переполнение
-        System.out.println("Переполнение значения int: " + OverValueInteger);
-        System.out.println("Переполнение значения double: " + overDouble);
         System.out.println("Переполнение значения float: " + overFloat);
+        System.out.println("Переполнение значения double: " + overDouble);
+        System.out.println("Переполнение значения int: " + OverValueInteger);
 
     }
 }
